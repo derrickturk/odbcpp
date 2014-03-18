b2002d31497793043bef2560d47b5ab212237a62
Derrick Turk <dwt@terminusdatascience.com>
2014-03-18 17:11:16 -0500

    Minor change to avoid a copy in test.

--- test.cpp
+++ test.cpp
@@ -15,7 +15,7 @@ int main()
     auto q = conn.make_query();
     q.execute("select * from dbo.snapshot_merge_WellMaster");
 
-    auto fs = q.fields();
+    auto&& fs = q.fields();
     for (const auto& f: fs) {
         std::cout << "Field: " << f.name << '\n'
             << "Type: " << type_name(f.type) << '\n';
