d4e6de01e49ae136044fdae5ad05c39369e5a6bf
Derrick Turk <dwt@terminusdatascience.com>
2014-03-20 13:36:11 -0500

    to_string and to_wstring

--- odbcpp_streams.hpp
+++ odbcpp_streams.hpp
@@ -1,19 +1,35 @@
 #ifndef ODBCPP_STREAMS_HPP
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
 #include "odbcpp.hpp"
 
 namespace odbcpp {
 
-std::ostream& operator<<(std::ostream& os, const odbcpp::datum& d);
+std::ostream& operator<<(std::ostream& os, const datum& d);
 
-std::wostream& operator<<(std::wostream& os, const odbcpp::datum& d);
+std::wostream& operator<<(std::wostream& os, const datum& d);
 
 template<class StreamType>
 inline StreamType& operator<<(StreamType& os, const data_type& t)
 {
     return os << type_name(t);
+}
+
+inline std::string to_string(const datum& d)
+{
+    std::stringstream s;
+    s << d;
+    return s.str();
+}
+
+inline std::wstring to_wstring(const datum& d)
+{
+    std::wstringstream s;
+    s << d;
+    return s.str();
 }
 
 }
