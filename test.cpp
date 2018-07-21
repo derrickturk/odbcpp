598d46d82105c7378136e2688e5355d4efc736cb
derrickturk <dwt@terminusdatascience.com>
2018-07-21 12:56:26 -0500

    Update test program.

--- test.cpp
+++ test.cpp
@@ -7,26 +7,35 @@
 #include <cassert>
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
     using namespace odbcpp;
 
-    connection conn("DRIVER=sql server;SERVER=VM-TXSQLDEV02\\BBE;Trusted Connection=Yes;Database=DSS_SFS;");
-
-    auto q = conn.make_query();
-    q.execute("select * from dbo.snapshot_merge_WellMaster");
-
-    if (q) {
-        for (unsigned i = 0; i < q.fields().size(); ++i)
-            std::cout << (i ? "\t" : "") << q.fields()[i].name << " :: "
-                << q.fields()[i].type;
-        std::cout << '\n';
+    if (argc != 2) {
+        std::cerr << "Usage: " << (argc ? argv[0] : "odbcpp")
+            << " connection-string\n";
+        return 0;
     }
 
-    while (q) {
-        for (unsigned i = 0; i < q.fields().size(); ++i)
-            std::cout << (i ? "\t" : "") << *(q.get(i));
-        std::cout << '\n';
-        q.advance();
-    }
+    char line[1024];
+    connection conn(argv[1]);
+
+    while (std::cin.getline(line, 1024)) {
+        auto q = conn.make_query();
+        q.execute(line);
+
+        if (q) {
+            for (unsigned i = 0; i < q.fields().size(); ++i)
+                std::cout << (i ? "\t" : "") << q.fields()[i].name << " :: "
+                    << q.fields()[i].type;
+            std::cout << '\n';
+        }
+
+        while (q) {
+            for (unsigned i = 0; i < q.fields().size(); ++i)
+                std::cout << (i ? "\t" : "") << *(q.get(i));
+            std::cout << '\n';
+            q.advance();
+        }
+    };
 }
